63cbefe9ae82ea39f35ff2ad350ace6724052ec5
Renzo Mendoza <rmendoza_2812@hotmail.com>
2024-09-22 21:21:57 -0500

    Creación del proyecto front

---
+++ src/main/java/pe/edu/cibertec/cl1_renzo_frontend/Cl1RenzoFrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1_renzo_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1RenzoFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1RenzoFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/cl1_renzo_frontend/Cl1RenzoFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1_renzo_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1RenzoFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
